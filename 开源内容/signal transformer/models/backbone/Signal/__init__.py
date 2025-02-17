44fa5ef5656224f852ad80989629d55adb76eba1
ranyeisking <93134582+ranyeisking@users.noreply.github.com>
2025-02-17 21:23:46 +0800

    Add files via upload

---
+++ 开源内容/signal transformer/models/backbone/Signal/__init__.py
@@ -0,0 +1,8 @@
+from .crossvit import cross_vit_tiny, cross_vit_big, cross_vit_base
+
+model_dict = {
+              'cross_vit_tiny':cross_vit_tiny,
+              'cross_vit_base':cross_vit_base,
+              'cross_vit_big':cross_vit_big
+
+}
